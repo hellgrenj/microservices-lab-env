77a9ddb07c8ceef5c4883a9c66f904433468f912
hellgrenj <hellgrenj@gmail.com>
2019-10-31 15:40:58 +0100

    played with the init script idea

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,31 @@
-console.log('this script will setup everything thats needed. including preparing ui-driven tests in scenarios?')
+const path = require('path')
+const sh = require('shelljs')
+
+sh.cd(path.join(__dirname, '../patterns'))
+if (sh.exec('npm install').code !== 0) {
+    sh.echo('failed to setup ./patterns');
+    sh.exit(1);
+}
+
+sh.cd(path.join(__dirname, '../scenarios'))
+if (sh.exec('npm install').code !== 0) {
+    sh.echo('failed to setup ./scenarios');
+    sh.exit(1);
+}
+
+sh.cd(path.join(__dirname, '../user-interface'))
+if (sh.exec('npm install').code !== 0) {
+    sh.echo('failed to setup ./user-interfaces');
+    sh.exit(1);
+}
+
+sh.cd(path.join(__dirname, '../user-interface/client'))
+if (sh.exec('npm install').code !== 0) {
+    sh.echo('failed to setup ./user-interfaces/client');
+    sh.exit(1);
+}
+
+// kanske om man skickar med --dev flagga?
+// alltid kolla om man har docker och docker-compose commandona installerade och i sin PATH?
+// också dotnet build i dot net core och go build i go?? 
+// med lite which koller i början om allt är installerat?
