a035385b47db9fe67773d796dfdb40c55c3578b5
hellgrenj <hellgrenj@gmail.com>
2019-11-14 14:36:38 +0100

    save commit; started to work on api gateway scenario

--- services/apigateway/server.js
+++ services/apigateway/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.get('/location', (req, res) => {
+app.get('/boss/location', (req, res) => {
   axios({
     method: 'get',
     url: process.env.BOSS_SERVICE_URL + 'location',
@@ -22,5 +22,38 @@ app.get('/location', (req, res) => {
       res.send(500, 'error')
     });
 })
+app.get('/deadlines/numberofmissed', (req, res) => {
+  axios({
+    method: 'get',
+    url: process.env.DEADLINES_SERVICE_URL + 'deadlines/numberofmissed',
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      // handle success
+      response.data.pipe(res)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      res.send(500, 'error')
+    });
+})
+app.get('/meetings/timetonextmeeting', (req, res) => {
+  axios({
+    method: 'get',
+    url: process.env.MEETINGS_SERVICE_URL + 'timetonextmeeting',
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      // handle success
+      response.data.pipe(res)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      res.send(500, 'error')
+    });
+})
+
 
 app.listen(port, () => console.log(`API Gateway listening on port ${port}!`))
--- user-interfaces/dashboard/client/src/views/Home.vue
+++ user-interfaces/dashboard/client/src/views/Home.vue
@@ -1,43 +1,97 @@
 <template>
   <div class="home">
     <Main msg="OSMAS Dashboard v 0.1" />
-    <div>
-     <span>Location of the boss: {{bossLocation}}</span>
+    <div class="container">
+      <div class="alert alert-dismissible alert-danger">
+        <strong>The boss is {{bossLocation}}</strong>
+      </div>
+      <div class="alert alert-dismissible alert-danger">
+        <strong>{{numberOfMissedDeadlines}}</strong>
+      </div>
+      <div class="alert alert-dismissible alert-danger">
+        <strong>{{timeToNextMeeting}}</strong>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 // @ is an alias to /src
-import Main from '@/components/Main.vue';
+import Main from "@/components/Main.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    Main,
+    Main
   },
   data() {
     return {
-      bossLocation: '',
+      bossLocation: "loading boss location....",
+      numberOfMissedDeadlines: 'loading number of missed deadlines',
+      timeToNextMeeting: "loading time to next meeting"
     };
   },
   methods: {
-    demo: async function demo() {
-      await axios
-        .get('http://localhost:8585/location')
-        .then((res) => {
-          console.log(res.data);
-          this.bossLocation = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    monitorTheBoss: function demo() {
+      setInterval(async () => {
+        await axios
+          .get("http://localhost:8585/boss/location")
+          .then(res => {
+            console.log(res.data);
+            this.bossLocation = res.data;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }, 3000);
     },
+    monitorDeadlines: function demo() {
+      setInterval(async () => {
+        await axios
+          .get("http://localhost:8585/deadlines/numberofmissed")
+          .then(res => {
+            console.log(res.data);
+            this.numberOfMissedDeadlines = res.data;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }, 10000);
+    },
+    monitorMeetings: function demo() {
+      setInterval(async () => {
+        await axios
+          .get("http://localhost:8585/meetings/timetonextmeeting")
+          .then(res => {
+            console.log(res.data);
+            this.timeToNextMeeting = res.data;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }, 5000);
+    }
   },
   async mounted() {
-    await this.demo();
-  },
+    this.monitorTheBoss();
+    this.monitorDeadlines();
+    this.monitorMeetings();
+  }
 };
 </script>
+
+<style scoped>
+.container {
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  text-align: center;
+}
+</style>
