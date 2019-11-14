4a7b39fa2456c4d8d0158b7792a23ab664fcb213
hellgrenj <hellgrenj@gmail.com>
2019-11-14 15:24:10 +0100

    added redis and spiked pub sub...

--- services/apigateway/server.js
+++ services/apigateway/server.js
@@ -3,10 +3,33 @@ const app = express()
 const port = process.env.PORT
 const axios = require('axios')
 const cors = require('cors')
+const Redis = require("ioredis");
+const redis = new Redis(6379, "redis");
+var pub = new Redis(6379, "redis");
+redis.subscribe("news", "music", function(err, count) {
+  // Now we are subscribed to both the 'news' and 'music' channels.
+  // `count` represents the number of channels we are currently subscribed to.
+
+  
+});
+
+redis.on("message", function(channel, message) {
+  // Receive message Hello world! from channel news
+  // Receive message Hello again! from channel music
+  console.log("Receive message %s from channel %s", message, channel);
+});
+
+// There's also an event called 'messageBuffer', which is the same as 'message' except
+// it returns buffers instead of strings.
+redis.on("messageBuffer", function(channel, message) {
+  // Both `channel` and `message` are buffers.
+});
 
 app.use(cors())
 
 app.get('/boss/location', (req, res) => {
+  pub.publish("news", "Hello world!");
+  pub.publish("music", "Hello again!");
   axios({
     method: 'get',
     url: process.env.BOSS_SERVICE_URL + 'location',
--- user-interfaces/dashboard/index.js
+++ user-interfaces/dashboard/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 const app = express();
+const Redis = require("ioredis");
+const redis = new Redis(6379, "redis");
+
+redis.subscribe("news", "music", function(err, count) {
+  // Now we are subscribed to both the 'news' and 'music' channels.
+  // `count` represents the number of channels we are currently subscribed to.
+});
+redis.on("message", function(channel, message) {
+  // Receive message Hello world! from channel news
+  // Receive message Hello again! from channel music
+  console.log("Receive message %s from channel %s", message, channel);
+});
+
+
 app.use(serveStatic(__dirname + "/client/dist"));
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
